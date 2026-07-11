dfdb7f9a7f4c71b5a0de261cfb7812b036260b7a
Nima <nima.hsh@gmail.com>
2026-07-11 21:52:05 +0330

    Fix

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
+import pandas as pd
 import streamlit as st
 
-st.write("1 - Streamlit OK")
+st.write("Loading Excel...")
 
-import pandas as pd
-st.write("2 - Pandas OK", pd.__version__)
+df = pd.read_excel("FirstSeason2026.xlsx")
 
-import numpy as np
-st.write("3 - NumPy OK", np.__version__)
+st.success("Excel Loaded")
 
-import plotly.express as px
-import plotly.graph_objects as go
-st.write("4 - Plotly OK")
-
-import jdatetime
-st.write("5 - jdatetime OK")
-
-st.success("All imports loaded successfully.")
+st.write(df.head())
